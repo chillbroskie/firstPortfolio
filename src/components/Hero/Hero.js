49cddfd7aae828e53e5881cc5d9d32604ce0f4b0
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-09-25 10:52:14 -0400

    hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Portfolio guided by JSmastery
+      </SectionTitle>
+      <SectionText>
+        My dream is to help build and create simple / sleek user experience on the FrontEnd aswell as how to support that on the backend server side.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
