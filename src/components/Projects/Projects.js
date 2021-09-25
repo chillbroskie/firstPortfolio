b1219a22f5150c3b0c98ccf4ebe5b1e5e4df47ac
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-09-25 11:04:29 -0400

    projects part

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,15 +4,22 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+
+
 const Projects = () => (
   <Section nopadding id='projects'>
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {[0, 1, 2, 3, 4, 5].map((project) => (
-        <div>
-          {project}
-        </div>
+      {projects.map(({id, image, title, description, tags, source, visit }) => (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+        </BlogCard>
       ))}
     </GridContainer>
   </Section>
